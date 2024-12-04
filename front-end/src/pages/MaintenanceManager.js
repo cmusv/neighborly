86c666ae26cce91e37cfde73ef121d62da7961c1
Yunxiao Xu <yunxiaox@andrew.cmu.edu>
2024-12-03 23:59:02 -0800

    Finish maintenance (manager)

--- front-end/src/pages/MaintenanceManager.js
+++ front-end/src/pages/MaintenanceManager.js
@@ -1,18 +1,45 @@
-import React from "react";
-import Header from "../components/MaintenanceRequests/Header";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Header from "../components/MaintenanceRequests/Header";
+import ActiveSelector from "../components/MaintenanceRequests/ActiveSelector";
+import RequestEntry from "../components/MaintenanceRequests/RequestEntry";
 
 const MaintenanceManager = () => {
     const navigate = useNavigate();
+    // initialize from local storage
+    const [requests, setRequests] = useState(JSON.parse(localStorage.getItem("requests")) || []);
+    const [activeTab, setActiveTab] = useState("unfinished");
+
+    // save to local storage
+    useEffect(() => {
+        localStorage.setItem("requests", JSON.stringify(requests));
+    }, [requests]);
+
     return (
         <>
             <Header
                 title="Maintenance Requests"
                 back={ {onClick: () => navigate("/"), label: "Home"} }
-                options={ [
-                    {label: "Edit", onClick: () => alert("Edit Clicked")}
-                ] }
+                // options={ [
+                //     {label: "Edit", onClick: () => alert("Edit Clicked")}
+                // ] }
             />
+            <ActiveSelector active={activeTab} setActive={setActiveTab} />
+            <div className="request-list">
+                {/* sort by isNew then priority */}
+                {Object.values(requests)
+                    .filter((request) => activeTab === "unfinished" ? !request.isFinished : request.isFinished)
+                    .sort((a, b) => b.isNew - a.isNew || a.priority - b.priority || a.id - b.id)
+                    .map((request, index) => (
+                        <RequestEntry
+                            key={index}
+                            title={request.title}
+                            priority={request.priority}
+                            isNew={request.isNew}
+                            onClick={() => navigate(`/maintenance-request/${request.id}`)}
+                        />
+                    ))}
+            </div>
         </>
     );
 };
