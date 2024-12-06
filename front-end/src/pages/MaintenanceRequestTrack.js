97b58c1b9a3d1dc50846231dad7dcb0c4fda542c
Yunxiao Xu <yunxiaox@andrew.cmu.edu>
2024-12-05 19:09:22 -0800

    Mark invalid input with red border

--- front-end/src/pages/MaintenanceRequestTrack.js
+++ front-end/src/pages/MaintenanceRequestTrack.js
@@ -134,7 +134,7 @@ const MaintenanceRequestTrack = () => {
                         type="tel"
                         name="contact"
                         placeholder="(000) 000-0000"
-                        className="input-field"
+                        className={`input-field ${formData.contact === "" || matchContact.test(formData.contact) ? "" : "invalid"}`}
                         value={formData.contact}
                         onChange={handleChange}
                     />
